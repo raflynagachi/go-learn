6e9151fcfa2fd83d07061e007a46e88708ed044c
raflynagachi <rafly.rigannagachi@gmail.com>
2022-04-06 08:21:40 +0700

    add Go Module exercise

---
+++ gomodule/goapp/go.mod
@@ -0,0 +1,5 @@
+module github.com/raflynagachi/goapp
+
+go 1.17
+
+require github.com/raflynagachi/golearn/v2 v2.0.0
---
+++ gomodule/goapp/main.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"fmt"
+
+	golearn "github.com/raflynagachi/golearn/v2"
+)
+
+func main() {
+	fmt.Println(golearn.Hello("rafly", "nagachi"))
+	fmt.Println(golearn.Welcome("nagachi"))
+}
