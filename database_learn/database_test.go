82c726b5bd2d0e59d20ddf1914fa313aea1a1ee0
raflynagachi <rafly.rigannagachi@gmail.com>
2022-04-09 17:47:32 +0700

    add Go database: ExecSQL & QuerySQL

--- database_learn/database_test.go
+++ database_learn/database_test.go
@@ -1,7 +1,9 @@
 package databaselearn
 
 import (
+	"context"
 	"database/sql"
+	"fmt"
 	"testing"
 
 	_ "github.com/go-sql-driver/mysql"
@@ -13,5 +15,41 @@ func TestConnection(t *testing.T) {
 
 	if err != nil {
 		panic(err)
+	}
+}
+
+func TestExecSql(t *testing.T) {
+	db := GetConnection()
+	defer db.Close()
+
+	ctx := context.Background()
+
+	query := "INSERT INTO customer(id, name) values ('go', 'Nagachi')"
+	_, err := db.ExecContext(ctx, query)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println("Success insert new customer")
+}
+
+func TestQuerySql(t *testing.T) {
+	db := GetConnection()
+	defer db.Close()
+
+	ctx := context.Background()
+	query := "SELECT id,name FROM customer"
+
+	rows, err := db.QueryContext(ctx, query)
+	if err != nil {
+		panic(err)
+	}
+	defer rows.Close()
+	for rows.Next() {
+		var id, name string
+		err := rows.Scan(&id, &name)
+		if err != nil {
+			panic(err)
+		}
+		fmt.Println(id, ":", name)
 	}
 }
