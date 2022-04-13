2087667aba5e14bc9c1c54467a1b6b93ae9b06a6
raflynagachi <rafly.rigannagachi@gmail.com>
2022-04-13 17:16:34 +0700

    add Go web: serve file

---
+++ goweb/serve_file_test.go
@@ -0,0 +1,52 @@
+package goweb
+
+import (
+	_ "embed"
+	"fmt"
+	"net/http"
+	"testing"
+)
+
+func ServeFile(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Query().Get("name") != "" {
+		http.ServeFile(w, r, "./resources/ok.html")
+	} else {
+		http.ServeFile(w, r, "./resources/notFound.html")
+	}
+}
+
+func TestServeFile(t *testing.T) {
+	server := http.Server{
+		Addr:    "localhost:8080",
+		Handler: http.HandlerFunc(ServeFile),
+	}
+	err := server.ListenAndServe()
+	if err != nil {
+		panic(err)
+	}
+}
+
+//go:embed resources/ok.html
+var resourcesOk string
+
+//go:embed resources/notFound.html
+var resourcesNotFound string
+
+func ServeFileEmbed(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Query().Get("name") != "" {
+		fmt.Fprint(w, resourcesOk)
+	} else {
+		fmt.Fprint(w, resourcesNotFound)
+	}
+}
+
+func TestServeFileEmbed(t *testing.T) {
+	server := http.Server{
+		Addr:    "localhost:8080",
+		Handler: http.HandlerFunc(ServeFileEmbed),
+	}
+	err := server.ListenAndServe()
+	if err != nil {
+		panic(err)
+	}
+}
