8ea82de20cbc3066a1effe08d524f07701d7d122
raflynagachi <rafly.rigannagachi@gmail.com>
2022-04-16 11:29:44 +0700

    add Go httprouter: middleware

---
+++ httprouter_learn/middleware_test.go
@@ -0,0 +1,41 @@
+package httprouterlearn
+
+import (
+	"fmt"
+	"io"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/julienschmidt/httprouter"
+	"github.com/stretchr/testify/assert"
+)
+
+type LogMiddleware struct {
+	Handler http.Handler
+}
+
+func (middleware *LogMiddleware) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("Receive request")
+	middleware.Handler.ServeHTTP(w, r)
+}
+
+func TestMiddleware(t *testing.T) {
+	router := httprouter.New()
+	router.GET("/", func(w http.ResponseWriter, r *http.Request, p httprouter.Params) {
+		fmt.Fprint(w, "Middleware")
+	})
+
+	middleware := LogMiddleware{router}
+
+	recorder := httptest.NewRecorder()
+	request := httptest.NewRequest(http.MethodGet, "http://localhost:8080/", nil)
+
+	middleware.ServeHTTP(recorder, request)
+
+	response := recorder.Result()
+	body, _ := io.ReadAll(response.Body)
+	fmt.Println(string(body))
+
+	assert.Equal(t, "Middleware", string(body))
+}
