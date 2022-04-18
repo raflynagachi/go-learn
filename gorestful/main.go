c4468f7e010cb7fb0d82bf8cfde8f52ee208b1f9
raflynagachi <rafly.rigannagachi@gmail.com>
2022-04-18 17:53:54 +0700

    add Go restful: router and server

---
+++ gorestful/main.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	_ "github.com/go-sql-driver/mysql"
+
+	"gorestful/app"
+	"gorestful/controller"
+	"gorestful/helper"
+	"gorestful/repository"
+	"gorestful/service"
+	"net/http"
+
+	"github.com/go-playground/validator"
+	"github.com/julienschmidt/httprouter"
+)
+
+func main() {
+	validate := validator.New()
+	db := app.NewDB()
+
+	categoryRepository := repository.NewCategoryRepository()
+	categoryService := service.NewCategoryService(categoryRepository, db, validate)
+	categoryController := controller.NewCategoryController(categoryService)
+
+	router := httprouter.New()
+	router.GET("/api/categories", categoryController.FindAll)
+	router.GET("/api/categories/:categoryId", categoryController.FindById)
+	router.POST("/api/categories/", categoryController.Create)
+	router.PUT("/api/categories/:categoryId", categoryController.Update)
+	router.DELETE("/api/categories/:categoryId", categoryController.Delete)
+
+	server := http.Server{
+		Addr:    "localhost:3000",
+		Handler: router,
+	}
+
+	err := server.ListenAndServe()
+	helper.PanicIfError(err)
+}
