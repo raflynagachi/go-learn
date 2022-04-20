3c55c27fd5dec86046219f3a609486bb911266af
raflynagachi <rafly.rigannagachi@gmail.com>
2022-04-20 22:26:02 +0700

    modify Go restful: chunking router

--- gorestful/main.go
+++ gorestful/main.go
@@ -5,7 +5,6 @@ import (
 
 	"gorestful/app"
 	"gorestful/controller"
-	"gorestful/exception"
 	"gorestful/helper"
 	"gorestful/middleware"
 	"gorestful/repository"
@@ -13,7 +12,6 @@ import (
 	"net/http"
 
 	"github.com/go-playground/validator"
-	"github.com/julienschmidt/httprouter"
 )
 
 func main() {
@@ -24,19 +22,12 @@ func main() {
 	categoryService := service.NewCategoryService(categoryRepository, db, validate)
 	categoryController := controller.NewCategoryController(categoryService)
 
-	router := httprouter.New()
-	router.GET("/api/categories", categoryController.FindAll)
-	router.GET("/api/categories/:categoryId", categoryController.FindById)
-	router.POST("/api/categories/", categoryController.Create)
-	router.PUT("/api/categories/:categoryId", categoryController.Update)
-	router.DELETE("/api/categories/:categoryId", categoryController.Delete)
+	router := app.NewRouter(categoryController)
 
 	server := http.Server{
 		Addr:    "localhost:3000",
 		Handler: middleware.NewAuthMiddleware(router),
 	}
-
-	router.PanicHandler = exception.ErrorHandler
 
 	err := server.ListenAndServe()
 	helper.PanicIfError(err)
