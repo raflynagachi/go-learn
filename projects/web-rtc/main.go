06b6e582f91cdd7b206f2603de3645127de146d0
raflynagachi <rafly.rigannagachi@gmail.com>
2023-06-01 22:35:15 +0700

    project: web rtc

---
+++ projects/web-rtc/main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"log"
+	"net/http"
+
+	"github.com/raflynagachi/web-rtc/server"
+)
+
+func main() {
+	server.AllRooms.Init()
+
+	http.HandleFunc("/create", server.CreateRoomRequestHandler)
+	http.HandleFunc("/join", server.JoinRoomRequestHandler)
+
+	log.Println("Starting server on Port 8000")
+	err := http.ListenAndServe(":8000", nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
