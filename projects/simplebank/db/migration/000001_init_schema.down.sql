1b7a572a7440f0ca09fe51fc352b9014a9e8d7ea
raflynagachi <rafly.rigannagachi@gmail.com>
2023-06-21 14:53:29 +0700

    feat: add users table

--- projects/simplebank/db/migration/000001_init_schema.down.sql
+++ projects/simplebank/db/migration/000001_init_schema.down.sql
@@ -1,3 +1,7 @@
+ALTER TABLE IF EXISTS "transfers" DROP CONSTRAINT IF EXISTS "transfers_from_account_id_to_account_id_fkey";
+ALTER TABLE IF EXISTS "transfers" DROP CONSTRAINT IF EXISTS "transfers_from_account_id_fkey";
+ALTER TABLE IF EXISTS "transfers" DROP CONSTRAINT IF EXISTS "transfers_to_account_id_fkey";
+ALTER TABLE IF EXISTS "entries" DROP CONSTRAINT IF EXISTS "entries_account_id_fkey";
 DROP TABLE IF EXISTS transfers;
 
 DROP TABLE IF EXISTS entries;
--- projects/simplebank/simplebank.sql
+++ projects/simplebank/simplebank.sql
@@ -1,30 +1,38 @@
-CREATE TABLE
-    "Accounts" (
-        "id" BIGSERIAL PRIMARY KEY,
-        "owner" varchar NOT NULL,
-        "balance" decimal NOT NULL,
-        "currency" varchar NOT NULL,
-        "created_at" timestamptz NOT NULL DEFAULT (now())
-    );
+CREATE TABLE "Users" (
+  "username" varchar PRIMARY KEY,
+  "hashed_password" varchar NOT NULL,
+  "full_name" varchar NOT NULL,
+  "email" varchar UNIQUE NOT NULL,
+  "password_changed_at" timestamptz NOT NULL DEFAULT (now()),
+  "created_at" timestamptz NOT NULL DEFAULT (now())
+);
 
-CREATE TABLE
-    "Entries" (
-        "id" BIGSERIAL PRIMARY KEY,
-        "account_id" bigint NOT NULL,
-        "amount" decimal NOT NULL,
-        "created_at" timestamptz NOT NULL DEFAULT (now())
-    );
+CREATE TABLE "Accounts" (
+  "id" BIGSERIAL PRIMARY KEY,
+  "owner" varchar NOT NULL,
+  "balance" decimal NOT NULL,
+  "currency" varchar NOT NULL,
+  "created_at" timestamptz NOT NULL DEFAULT (now())
+);
 
-CREATE TABLE
-    "Transfers" (
-        "id" BIGSERIAL PRIMARY KEY,
-        "from_account_id" bigint NOT NULL,
-        "to_account_id" bigint NOT NULL,
-        "amount" decimal NOT NULL,
-        "created_at" timestamptz NOT NULL DEFAULT (now())
-    );
+CREATE TABLE "Entries" (
+  "id" BIGSERIAL PRIMARY KEY,
+  "account_id" bigint NOT NULL,
+  "amount" decimal NOT NULL,
+  "created_at" timestamptz NOT NULL DEFAULT (now())
+);
+
+CREATE TABLE "Transfers" (
+  "id" BIGSERIAL PRIMARY KEY,
+  "from_account_id" bigint NOT NULL,
+  "to_account_id" bigint NOT NULL,
+  "amount" decimal NOT NULL,
+  "created_at" timestamptz NOT NULL DEFAULT (now())
+);
 
 CREATE INDEX ON "Accounts" ("owner");
+
+CREATE UNIQUE INDEX ON "Accounts" ("owner", "currency");
 
 CREATE INDEX ON "Entries" ("account_id");
 
@@ -38,14 +46,10 @@ COMMENT ON COLUMN "Entries"."amount" IS 'can be positive or negative';
 
 COMMENT ON COLUMN "Transfers"."amount" IS 'must be positive';
 
-ALTER TABLE "Entries"
-ADD
-    FOREIGN KEY ("account_id") REFERENCES "Accounts" ("id");
+ALTER TABLE "Entries" ADD FOREIGN KEY ("account_id") REFERENCES "Accounts" ("id");
 
-ALTER TABLE "Transfers"
-ADD
-    FOREIGN KEY ("from_account_id") REFERENCES "Accounts" ("id");
+ALTER TABLE "Transfers" ADD FOREIGN KEY ("from_account_id") REFERENCES "Accounts" ("id");
 
-ALTER TABLE "Transfers"
-ADD
-    FOREIGN KEY ("to_account_id") REFERENCES "Accounts" ("id");
+ALTER TABLE "Transfers" ADD FOREIGN KEY ("to_account_id") REFERENCES "Accounts" ("id");
+
+ALTER TABLE "Accounts" ADD FOREIGN KEY ("owner") REFERENCES "Users" ("username");
